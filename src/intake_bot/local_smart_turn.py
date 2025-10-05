d7c23afda53a24fb76fcb9855d67fb15b3fa3d2a
Jack Adamson <jackadamson@gmail.com>
2025-10-05 17:08:38 -0500

    fix: cleaned up local smart turn init

--- src/intake_bot/local_smart_turn.py
+++ src/intake_bot/local_smart_turn.py
@@ -3,15 +3,15 @@ import sys
 from .env_var import env_var_is_true
 from .server import logger
 
-if not env_var_is_true("DISABLE_LOCAL_SMART_TURN"):
+if env_var_is_true("DISABLE_LOCAL_SMART_TURN"):
+    logger.info("LocalSmartTurnAnalyzerV3 is not enabled.")
+    turn_analyzer = None
+else:
     try:
         from pipecat.audio.turn.smart_turn.local_smart_turn_v3 import LocalSmartTurnAnalyzerV3
 
         turn_analyzer = LocalSmartTurnAnalyzerV3()
     except Exception:
         sys.exit(
-            "[INFO] intake-bot: You can also use `uv sync --group lst` to install the modules for LocalSmartTurnAnalyzerV3"
+            "[INFO] intake-bot: You are missing the module 'LocalSmartTurnAnalyzerV3'. You use `uv sync` to install the module."
         )
-else:
-    logger.info("LocalSmartTurnAnalyzerV3 is not enabled.")
-    turn_analyzer = None
