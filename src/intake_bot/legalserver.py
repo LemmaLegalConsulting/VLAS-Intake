53c2802f2c4f04b8ad8ab3b7ac2d0070107018cb
Jack Adamson <jackadamson@gmail.com>
2025-10-09 10:23:35 -0500

    wip legalserver api

--- src/intake_bot/legalserver.py
+++ src/intake_bot/legalserver.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
-API_URL = f"""https://{os.getenv("LEGAL_SERVER_SUBDOMAIN")}.legalserver.org/api/v1/"""
+API_URL = f"""https://{os.getenv("LEGAL_SERVER_SUBDOMAIN")}.legalserver.org/api/v2/"""
 headers = {
     "Authorization": f"""Bearer {os.getenv("LEGAL_SERVER_BEARER_TOKEN")}""",
     "Content-Type": "application/json",
@@ -19,7 +19,7 @@ def test_get(endpoint_url: str, headers: dict):
         params = {
             "page_number": 1,
             "page_size": 1,
-            "first": "Celeste",
+            "first": "Dexter",
             "last": "Campbell",
         }
         response = requests.get(url, headers=headers, params=params)
@@ -40,22 +40,23 @@ def test_post(endpoint_url: str, headers: dict):
         # }
 
         payload = {
-            "first": "Celeste",
+            "first": "Dexter",
             "last": "Campbell",
-            "middle": "Caroline",
+            "middle": "",
             "is_group": False,
             "case_disposition": "Incomplete Intake",
             "mobile_phone": "8665345243",
-            "mobile_phone_safe": True,
-            "county_of_dispute": {
-                "county_name": "Amelia",
-                "county_state": "VA",
-            },
-            "percentage_of_poverty": "0%",
-            "asset_eligible": True,
-            "lsc_eligible": True,
-            "income_eligible": True,
-            "victim_of_domestic_violence": True,
+            # "mobile_phone_safe": True,
+            # "county_of_dispute": {
+            #     "county_name": "Amelia",
+            #     "county_state": "VA",
+            # },
+            # "percentage_of_poverty": "0%",
+            # "asset_eligible": True,
+            # "lsc_eligible": True,
+            # "income_eligible": True,
+            # "victim_of_domestic_violence": True,
+            "legal_problem_code": "91 Legal Assist. to Non-Profit Org. or Group (Incl. Incorp./Diss.)",
         }
 
         response = requests.post(url, headers=headers, json=payload)
@@ -70,8 +71,8 @@ def test_conflict_check(endpoint_url: str, headers: dict):
     url = API_URL + endpoint_url
     try:
         payload = {
-            "first": "Jimmy",
-            "last": "Dean",
+            "first": "Dexter",
+            "last": "Campbell",
         }
         response = requests.post(url, headers=headers, json=payload)
         if response.status_code != 200:
@@ -82,8 +83,8 @@ def test_conflict_check(endpoint_url: str, headers: dict):
 
 
 if __name__ == "__main__":
-    # test_post("matters", headers)
+    test_post("matters", headers)
     # print()
     # test_get("matters", headers)
     # print()
-    test_conflict_check("conflict_check", headers)
+    # test_conflict_check("conflict_check", headers)
